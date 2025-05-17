65d8faecf9ed59fb1447504ec79d433346ce1414
haider-zaidi72 <suhz1100@gmail.com>
2025-05-17 09:45:51 -0700

    add

--- java.js
+++ java.js
@@ -1,9 +1,17 @@
+ function validateForm() {
+            // Get all input values
+            const firstName = document.getElementById("firstName").value;
+            const lastName = document.getElementById("lastName").value;
+            const age = document.getElementById("age").value;
+            const cnic = document.getElementById("cnic").value;
+            const roll = document.getElementById("roll").value;
+            const message = document.getElementById("message").value;
 
-let text=document.getElementById("a")
-text.style.backgroundColor=("orange");
-
-
-function click () {
-    let text=document.getElementById("ab")
-    text.style.color=("red");
-}
+            // Check if any field is empty
+            if (!firstName || !lastName || !age || !cnic || !roll || !message) {
+                alert("Please fill all empty fields");
+            } else {
+                alert("Form submitted successfully!");
+               
+            }
+        }
