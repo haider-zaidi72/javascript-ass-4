bd77b7ab1afa4b901c7bf21ae98a21fdc7bc50b8
urooj haider <suhz1100@gmail.com>
2025-05-17 18:20:26 +0500

    add

---
+++ java.js
@@ -0,0 +1,9 @@
+
+let text=document.getElementById("a")
+text.style.backgroundColor=("orange");
+
+
+function click () {
+    let text=document.getElementById("ab")
+    text.style.color=("red");
+}
