84f63a1e329f4b4db3b72e909ff14ef3ad693069
haider-zaidi72 <suhz1100@gmail.com>
2025-05-18 02:59:31 -0700

    add

--- java.js
+++ java.js
@@ -1,11 +1,11 @@
  function validateForm() {
             // Get all input values
-            const firstName = document.getElementById("firstName").value;
-            const lastName = document.getElementById("lastName").value;
-            const age = document.getElementById("age").value;
-            const cnic = document.getElementById("cnic").value;
-            const roll = document.getElementById("roll").value;
-            const message = document.getElementById("message").value;
+            let firstName = document.getElementById("firstName").value;
+            let lastName = document.getElementById("lastName").value;
+            let age = document.getElementById("age").value;
+            let cnic = document.getElementById("cnic").value;
+            let roll = document.getElementById("roll").value;
+            let message = document.getElementById("message").value;
 
             // Check if any field is empty
             if (!firstName || !lastName || !age || !cnic || !roll || !message) {
@@ -34,3 +34,15 @@ function generateRandomNumber() {
             const randomNum = Math.round(Math.random() * 100) + 1;
             document.getElementById("randomNumber").textContent = randomNum;
             }
+
+function Credential (){
+    // get all input values
+    let userID =document.getElementById("userID").value
+    let passWord = document.getElementById("passWord").value
+ // check if fields are not filled
+ if (!userID || !passWord){
+    alert("Please enter the user ID and Password");
+ } else{
+    alert("Your are logedin");
+ }
+}
