61afba2753b4af763aa74db2d328558963c0c5bc
haider-zaidi72 <suhz1100@gmail.com>
2025-05-18 02:39:31 -0700

    add

--- java.js
+++ java.js
@@ -15,3 +15,22 @@
                
             }
         }
+
+
+
+
+function clrChange(){
+let r = Math.round(Math.random()*255)
+let g = Math.round(Math.random()*255)
+let b = Math.round(Math.random()*255)
+
+    let text=document.getElementById("clr")
+     text.style.backgroundColor=`rgb(${r},${g},${b})`;
+
+
+}
+function generateRandomNumber() {
+            // Generate random number between 1 and 100
+            const randomNum = Math.round(Math.random() * 100) + 1;
+            document.getElementById("randomNumber").textContent = randomNum;
+            }
